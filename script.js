cd287d8641969b2de21e34786d18f3e4cae41a36
btousifar <bn.tousifar86@gmail.com>
2025-01-02 20:52:45 -0600

    Create dropToken function
    in the Gameboard function I added a new method
    called dropToken which take three values as
    row, column and player. then using board array
    it will targer specific place and put the player
    token in the spot. if the spot is already full,
    then it prints the message that the spot is not
    empty.
    
    [Ticket: 2]

--- script.js
+++ script.js
@@ -9,7 +9,20 @@ function Gameboard() {
             board[i].push(Cell());
          }
     }
-    console.log(board)
+    // console.log(board[0][0].addToken('X'));
+
+    const dropToken = (row, column, player) => {
+        // board.map((row) => row.map((cell) => cell.getValue() === 0).map((cell) => cell.addToken(player)));
+        const isEmpthySpot = board[row][column];
+        isEmpthySpot.getValue() === 0 ? isEmpthySpot.addToken(player) : console.log('not empty');
+    }
+
+    const printBoard = () => {
+        const boardCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardCellValues);
+    };
+
+    return {dropToken, printBoard};
 };
 
 function Cell() {
@@ -25,7 +38,23 @@ function Cell() {
         addToken,
         getValue
     };
-}
+};
 
-Gameboard();
+
+
+
+
+
+
+
+
+const game = Gameboard();
+const row = prompt("select row: ");
+const column = prompt("select column: ");
+game.dropToken(row,column,"X");
+game.dropToken(2,2,"O");
+game.dropToken(2,2,"O");
+game.printBoard();
+
+
 
