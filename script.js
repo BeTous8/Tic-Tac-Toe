1c8c298261d2aafceabbaf5cab46d57d6c53fb4f
btousifar <bn.tousifar86@gmail.com>
2025-01-04 14:00:54 -0600

    Build gameController function
    this is where the player array is created. inside
    each one there is an object with name and token
    attributes. then activePlayer is defined based on
    the current player. Also switchPlayer will toggle
    players on each round. At the end playRound
    function is defined to place the mark of each
    player (token) either "X" or "O" on the board and
    finally print the board on the console.
    
    [Ticket: 4]

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Gameboard() {
+const Gameboard = function () {
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -9,7 +9,9 @@ function Gameboard() {
             board[i].push(Cell());
          }
     }
-    // console.log(board[0][0].addToken('X'));
+
+    // give the board array to UI to render it on the webpage
+    const getBoard = () => board;
 
     const dropToken = (row, column, player) => {
         // board.map((row) => row.map((cell) => cell.getValue() === 0).map((cell) => cell.addToken(player)));
@@ -22,7 +24,14 @@ function Gameboard() {
         console.log(boardCellValues);
     };
 
-    return {dropToken, printBoard};
+    const checkEmptyCells = () => {
+        const emptyCells = board.flatMap((row) => row.filter((cell) => cell.getValue() === 0));
+        console.log(emptyCells.length); // Number of empty cells
+        return emptyCells.length > 0;
+      };
+      
+
+    return {getBoard, dropToken, printBoard, checkEmptyCells};
 };
 
 function Cell() {
@@ -42,19 +51,85 @@ function Cell() {
 
 
 
+function gameController(
+    playerOneName = 'player 1',
+    playerTwoName = 'player 2',
+) {
+
+    const board = Gameboard();
+    const player = [
+        {
+            name : playerOneName,
+            token : "X"
+        },
+        {
+            name : playerTwoName,
+            token : "O"
+        }
+    ]
+
+    let currentPlayer = player[0];
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player[0] ? player[1] : player[0];
+    }
+
+    const getActivePlayer = () => currentPlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+      };
+
+    const playRound = (row, column) => {
+        board.dropToken(row,column, getActivePlayer().token);
+        // check for the winner or it there is no cell remains
+        // gameIsOver();
+
+        switchPlayer();
+        printNewRound();
+    }
+
+    
+    // we are going to remove while and put addevent listener later
+    const gameIsOver = () => {
+        if (board.checkEmptyCells()) {
+            }
+        }  
+        
+        //checking for all winning 3-in-a-rows
+
+    return {playRound, switchPlayer, getActivePlayer};
+
+};
+
+
+const game = gameController();
+const instance1 = game.playRound(0, 0);
+const instance2 = game.playRound(1, 1);
+const instance3 = game.playRound(1, 0);
+const instance4 = game.playRound(1, 2);
+const instance5 = game.playRound(2, 0);
+
+
+// console.log(instance1)
+
+// const displayGameboard = (function() {
+//     // Code to be executed
+//   })(); 
 
 
 
 
+// const game = Gameboard();
+// const row = prompt("select row: ");
+// const column = prompt("select column: ");
+// game.dropToken(row,column,"X");
+// game.dropToken(2,2,"O");
+// game.dropToken(2,2,"O");
+// game.printBoard();
 
 
-const game = Gameboard();
-const row = prompt("select row: ");
-const column = prompt("select column: ");
-game.dropToken(row,column,"X");
-game.dropToken(2,2,"O");
-game.dropToken(2,2,"O");
-game.printBoard();
 
 
 
